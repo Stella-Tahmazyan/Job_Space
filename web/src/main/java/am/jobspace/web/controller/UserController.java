a33b57f085444972256a74d8754c06299ad82f5d
stellahahmazyan <stella.tahmazyan@scdm.de>
2019-04-09 18:20:02 +0400

    fix api methodd

--- web/src/main/java/am/jobspace/web/controller/UserController.java
+++ web/src/main/java/am/jobspace/web/controller/UserController.java
@@ -1,20 +1,32 @@
 package am.jobspace.web.controller;
 
 
+import am.jobspace.common.model.Gender;
 import am.jobspace.common.model.User;
+import am.jobspace.common.model.UserType;
 import am.jobspace.common.repository.UserRepository;
 import am.jobspace.common.service.EmailService;
+import am.jobspace.web.security.SpringUser;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -32,12 +44,9 @@ public class UserController {
 
   @Value("${image.upload.dir}")
   private String imageUploadDir;
-//  @Autowired
-//  private Facebook facebook;
-//  @Autowired
-//  private ConnectionRepository connectionRepository;
-//  @Autowired
-//  private PasswordEncoder passwordEncoder;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   @Autowired
   private UserRepository userRepository;
@@ -46,23 +55,22 @@ public class UserController {
 //  private EmailService emailService;
 
   @GetMapping("/loginSuccess")
-  public String loginSuccess(/*@AuthenticationPrincipal
-      SpringUser springUser, */HttpServletRequest request) {
-//    if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
-//      return "redirect:/connect/facebook";
-//    }
-//    org.springframework.social.facebook.api.User user = facebook.userOperations().getUserProfile();
-//
-//    User user1 = new User(user.getFirstName(), user.getLastName(), "0444",user.getFirstName()+"."+user.getLastName()+".gmail.com",
-//        Gender.FEMALE, "facebook", UserType.EMPLOYER,"dddd");
-//    userRepository.save(springUser.getUser());
-////    request.getSession().setAttribute("user", user != null ? user : springUser.getUser());
-//    if (springUser != null) {
-//      if (springUser.getUser().getUserType() == UserType.EMPLOYER) {
-//        return "redirect:/employer";
-//      }
-//    }
-    return "redirect:/";
+  public String loginSuccess(@AuthenticationPrincipal
+      SpringUser springUser, HttpServletRequest request) {
+    String url = "http://localhost:8085/user/add";
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<User> response = restTemplate.exchange(
+      url,
+      HttpMethod.GET,
+      null,
+      new ParameterizedTypeReference<User>() {
+      });
+
+   User user = response.getBody();
+   if(user!=null){
+     return "/";
+   }
+    return "redirect:/register";
 
   }
 
@@ -76,6 +84,20 @@ public class UserController {
   @PostMapping("/register")
   public String register(RedirectAttributes redirectAttributes,@ModelAttribute User user,
       @RequestParam("picture") MultipartFile file,BindingResult buldingResult) throws IOException {
+    String url = "http://localhost:8085/user/add";
+    RestTemplate restTemplate = new RestTemplate();
+    RequestEntity<User> response = restTemplate.postForEntity(user);
+        url,
+        HttpMethod.POST,
+        user,
+  });
+
+    User user = response.getBody();
+    if(user!=null){
+      return "/";
+    }
+    return "redirect:/register";
+
 //    user.setPassword(passwordEncoder.encode(user.getPassword()));
     String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
     File picture = new File(imageUploadDir + File.separator + fileName);
